f62f106818bc028e8685c34f0b548f24cb184021
m-e-h <info@martyhelmick.com>
2015-05-01 17:52:45 -0400

    fresh theme

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package    hmavl-child
+ * @author     Marty Helmick <info@martyhelmick.com>
+ * @copyright  Copyright (c) 2015, Diocese of Charlotte
+ * @link       https://github.com/DioceseOfCharlotte/hmavl-child
+ * @license    http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+/* Add the child theme setup function to the 'after_setup_theme' hook. */
+add_action( 'after_setup_theme', 'hmavl_child_theme_setup' );
+
+/**
+ * Setup function.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
+function hmavl_child_theme_setup() {
+}
